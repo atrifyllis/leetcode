6f2590b5c46f9639f4b8b1bbe459ad795accf975
atrifyllis <atrifyllis@gmail.com>
2023-01-04 22:50:51 +0200

    Fix Longest Substring Without Repeating Characters.

--- src/main/kotlin/gr/alx/leetcode/lengthoflongestsubstring/Solution.kt
+++ src/main/kotlin/gr/alx/leetcode/lengthoflongestsubstring/Solution.kt
@@ -2,17 +2,28 @@ package gr.alx.leetcode.lengthoflongestsubstring
 
 class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
-        val input: List<String> = s.split("").filter { !it.isEmpty() }
+        val input: List<String> = s.split("").filter { it.isNotEmpty() }
+
+        val charToIndexMap = HashMap<String, Int>()
 
         var leftPointer = 0
         var rightPointer = 0
-        var currentSubString = HashSet<String>()
+        var maxLength = 0
 
-        for (leftPointer in 0 until input.size) {
-            if (currentSubString.add(input[rig htPointer])) {
-                rightPointer++
+        for (rightPointer in input.indices) {
+            val currentChar: String = input[rightPointer]
+            val currentCharIndex: Int? = charToIndexMap[currentChar]
+            // if we have already seen this character,
+            // move left pointer to the right of the current character (avoid consecutive duplicates)
+            if (currentCharIndex != null) {
+                leftPointer = maxOf(leftPointer, currentCharIndex + 1)
             }
-        }
+            // always set or replace currentChar index in hash map
+            charToIndexMap[currentChar] = rightPointer
+            // since we already moved leftPointer, we add 1 to find the length.
+            maxLength = maxOf(rightPointer - leftPointer + 1, maxLength)
 
+        }
+        return maxLength
     }
 }
